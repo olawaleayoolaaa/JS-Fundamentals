7efd324e738edd7f7d8383c703ef59372ee6d47d
Olawale Ayoola <olawaleopeyemi5303@gmail.com>
2025-07-16 04:56:52 +0100

    made changes

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,10 @@
 let argument;
 if (argument = 0){
-    console.log("No argument")
+    console.log("No argument");
 }
 else if (argument = 1){
-    console.log("Argument found")
+    console.log("Argument found");
 }
-if (argument > 1){
-    console.log("Arguments found")
+else {
+    console.log("Arguments found");
 }
