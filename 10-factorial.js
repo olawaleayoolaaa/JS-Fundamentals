02de10181cda70c12b3c6ab8b89dbd0db1f7c251
Olawale Ayoola <olawaleopeyemi5303@gmail.com>
2025-07-16 07:53:30 +0100

    updated

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,19 +1,9 @@
-const inp = parseInt(process.argv[2]);
-
-function factorial(num){
-    if (num === NaN){
-        console.log("1")
-    }
-
-    else{
-    let i=num
-    let j=1;
-    while (i>0){
-    j*=i
-    i--
-    }
-    console.log(j)
-    }
+function factorial(n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
-factorial(inp);
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
