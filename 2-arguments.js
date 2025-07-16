d7c7d8d81b8aaa47567c050bdfa1fbbd38a68a51
Olawale Ayoola <olawaleopeyemi5303@gmail.com>
2025-07-16 04:52:50 +0100

    created new script

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+let argument;
+if (argument = 0){
+    console.log("No argument")
+}
+else if (argument = 1){
+    console.log("Argument found")
+}
+if (argument > 1){
+    console.log("Arguments found")
+}
