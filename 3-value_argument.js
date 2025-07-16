caf7d218ef87558d127fbdcb5aa22c9fdec22847
Olawale Ayoola <olawaleopeyemi5303@gmail.com>
2025-07-16 06:07:36 +0100

    fixed bugs

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,7 +1,7 @@
-const args = process.argv.slice(1);
-if (args.length === 0) {
+const args = process.argv.slice(2);
+
+if (args[0] === undefined) {
   console.log("No argument");
-} 
- else {
-  console.log(...args);
+} else {
+  console.log(args[0]);
 }
