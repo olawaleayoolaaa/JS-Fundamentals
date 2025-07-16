bb224973f3fbe79d172356117592619787714ca6
Olawale Ayoola <olawaleopeyemi5303@gmail.com>
2025-07-16 05:06:59 +0100

    updated errors

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,8 @@
 let argument;
-if (argument = 0){
+if (argument === 0){
     console.log("No argument");
 }
-else if (argument = 1){
+else if (argument === 1){
     console.log("Argument found");
 }
 else {
