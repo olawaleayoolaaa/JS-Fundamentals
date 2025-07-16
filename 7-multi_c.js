e0681c91ab9013cf3802abdc70c6c1d507834bf4
Olawale Ayoola <olawaleopeyemi5303@gmail.com>
2025-07-16 06:37:21 +0100

    new script

---
+++ 7-multi_c.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2);
+if (Number.parseInt(args[0])){
+    for(let i=0;1<Number.parseInt(args[0]);i++){
+        console.log("C is fun")
+    }
+}
+else{
+    console.log("Missing number of occurrences")
+}
