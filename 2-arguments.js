9ea80ed58e90c303a151d6cd0e45ceb62d32f6df
Olawale Ayoola <olawaleopeyemi5303@gmail.com>
2025-07-16 05:12:55 +0100

    commented

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,10 @@
 let argument;
-if (argument == 0){
+if (argument === 0){
     console.log("No argument");
 }
-else if (argument === 1){
-    console.log("Argument found");
-}
+// else if (argument === 1){
+//     console.log("Argument found");
+// }
 else {
     console.log("Arguments found");
 }
