d077b1011fba9a6442d22111a09b5e71a4ea08fa
Olawale Ayoola <olawaleopeyemi5303@gmail.com>
2025-07-16 06:54:36 +0100

    New script

---
+++ 8-square.js
@@ -0,0 +1,12 @@
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
+} else {
+ for (let i=0;i<x;i++){
+    console.log("X")
+    for (let j=0;j<x;j++)(
+        console.log("X")
+    )
+ }
+}
