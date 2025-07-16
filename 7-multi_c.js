92e5ccebf725b3de29ee87c0f207e474567fb44d
Olawale Ayoola <olawaleopeyemi5303@gmail.com>
2025-07-16 06:39:50 +0100

    fixed bugs

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,6 +1,6 @@
 const args = process.argv.slice(2);
 if (Number.parseInt(args[0])){
-    for(let i=0;1<Number.parseInt(args[0]);i++){
+for (let i=0;1<Number.parseInt(args[0]);i++){
         console.log("C is fun")
     }
 }
