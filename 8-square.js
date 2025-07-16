f7c44ac9e762cefe22a8d0d76d7deb9ae00d3ad4
Olawale Ayoola <olawaleopeyemi5303@gmail.com>
2025-07-16 07:00:22 +0100

    updated

--- 8-square.js
+++ 8-square.js
@@ -4,9 +4,6 @@ if (isNaN(x)) {
   console.log("Missing number of occurrences");
 } else {
  for (let i=0;i<x;i++){
-    console.log("X")
-    for (let j=0;j<x;j++)(
-        console.log("X")
-    )
+    console.log(`${"X".repeat(x)}`)
  }
 }
